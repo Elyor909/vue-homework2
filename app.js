54bde283019da33032c35957e455d4f5b408a45c
Elyor <nuridinove2001@gmail.com>
2023-03-02 23:45:38 +0500

    vue-homework2

---
+++ app.js
@@ -0,0 +1,50 @@
+
+let id = 1;
+const app = {
+    data() {
+        return {
+            title: 'Выберите категорию',
+            text: '',
+            game: '',
+            csgo: [
+                { name: 'John' },
+                { name: 'Alex' },
+                { name: 'Sasha' },
+            ],
+            dota: [
+                { name: 'Denis' },
+                { name: 'Leon' },
+            ],
+            pubg: [
+                { name: 'David' },
+                { name: 'Kolya' },
+            ]
+        }
+    },
+    methods: {
+        delUser(user) {
+            this.csgo = this.csgo.filter(item => item.name != user.name)
+            this.dota = this.dota.filter(item => item.name != user.name) 
+            this.pubg = this.pubg.filter(item => item.name != user.name)
+        },
+        addUser() {
+            if(this.game == 'csgo' && this.text.length > 1) {
+                this.csgo.push({
+                    name: this.text
+                })
+            } else if(this.game == 'dota' && this.text.length > 1) {
+                this.dota.push({
+                    name: this.text
+                })     
+            } else if(this.game == 'pubg' && this.text.length > 1) {
+                this.pubg.push({
+                    name: this.text
+                })
+            }
+            this.text = ''
+            this.game = ''
+        },
+    }
+}
+
+Vue.createApp(app).mount('#app')
